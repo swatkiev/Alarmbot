ecc5744106e24c5b5bbd6c246368422afb8446e8
swatkiev <swatkiev@gmail.com>
2023-08-10 11:45:32 +0300

    new changes

--- client.py
+++ client.py
@@ -7,13 +7,6 @@ api_hash = 'PUT HERE YOUR API_HASH FROM API TELEGRAM'
 client = TelegramClient('your_sesion_name', api_id, api_hash)
 client.start()
 
-client.send_message('@your_bot_name', '/renew')
-time.sleep(10)
-client.send_message('@your_bot_name', '/renew')
-time.sleep(10)
-client.send_message('@your_bot_name', '/renew')
-time.sleep(10)
-client.send_message('@your_bot_name', '/renew')
-time.sleep(10)
-client.send_message('@your_bot_name', '/renew')
-time.sleep(10)
+for number in range(5):
+    client.send_message('@your_bot_name', '/renew')
+    time.sleep(10)
